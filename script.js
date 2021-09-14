7f7c22f6158b16c5e2e7afbc256399c081173394
Evgeniy3171 <evgen915628@list.ru>
2021-09-14 20:36:14 +0300

    Сократил код

--- script.js
+++ script.js
@@ -7,33 +7,63 @@ let buttonPlus = document.getElementById('buttonPlus'),
     buttonClear = document.getElementById('cleaner'),
     result;
 
-function onButtonPlusClick() {
-    result = +inputFirst.value + +inputSecond.value;
+function makeOperation(operand) {   //в зависимости от переданного операнда происходит соответствующее вычисление
+    if (operand === '+') {
+        result = +inputFirst.value + +inputSecond.value;
+    } else if (operand === '-') {
+        result = +inputFirst.value - +inputSecond.value;
+    } else if (operand === '*') {
+        result = +inputFirst.value * +inputSecond.value;
+    } else {
+        result = +inputFirst.value / +inputSecond.value;
+    }
     console.log('result: ', result);
+}
+/* первый вариант */
+/* function onButtonPlusClick() {
+    // result = +inputFirst.value + +inputSecond.value;
+    // console.log('result: ', result);
+    makeOperation('+');
 }
 
 function onButtonMinusClick() {
-    result = +inputFirst.value - +inputSecond.value;
-    console.log('result: ', result);        
+    // result = +inputFirst.value - +inputSecond.value;
+    // console.log('result: ', result);
+    makeOperation('-');        
 }
 
 function onButtonMultplayClick() {
-    result = +inputFirst.value * +inputSecond.value;
-    console.log('result: ', result);        
+    // result = +inputFirst.value * +inputSecond.value;
+    // console.log('result: ', result); 
+    makeOperation('*');       
 }
 
 function onButtonDevideClick() {
-    result = +inputFirst.value / +inputSecond.value;
-    console.log('result: ', result);        
-}
+    // result = +inputFirst.value / +inputSecond.value;
+    // console.log('result: ', result);
+    makeOperation('/');        
+} */
 
 function onCleanerClick() {
     inputFirst.value = "";
     inputSecond.value = "";
 }
 
-buttonPlus.addEventListener('click', onButtonPlusClick);
+function onOperationButtonClick(eventObject) {
+    const clickedElement = eventObject.currentTarget,
+          operation = clickedElement.innerHTML;
+    makeOperation(operation);
+}
+
+/* buttonPlus.addEventListener('click', onButtonPlusClick);
 buttonMinus.addEventListener('click', onButtonMinusClick);
 buttonMultplay.addEventListener('click', onButtonMultplayClick);
 buttonDevide.addEventListener('click', onButtonDevideClick);
+buttonClear.addEventListener('click', onCleanerClick);
+ */
+
+buttonPlus.addEventListener('click', onOperationButtonClick);
+buttonMinus.addEventListener('click', onOperationButtonClick);
+buttonMultplay.addEventListener('click', onOperationButtonClick);
+buttonDevide.addEventListener('click', onOperationButtonClick);
 buttonClear.addEventListener('click', onCleanerClick);
